38975b114c0c133fcb11e47ac2937c858b81084b
AMarinaG <alberto.marina.gonzalo@gmail.com>
2025-01-20 19:15:15 +0100

    feat: update all versionsand kotlin to 2.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.test) apply false
+    alias(libs.plugins.compose) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.hilt) apply false
